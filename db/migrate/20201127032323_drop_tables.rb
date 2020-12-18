413fae8fc06949ee1f9b7886f0bc447b1ce03b7d
むちりす <mucchirisu@gmail.com>
2020-12-18 22:31:58 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20201127032323_drop_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropTables < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :prousers
-    drop_table :users
-    drop_table :comments
-    drop_table :likes
-    drop_table :posts
-  end
-end
