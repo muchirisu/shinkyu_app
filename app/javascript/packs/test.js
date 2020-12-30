30b6ec4e9925900f2263ecda6e2c3e2c054f880e
むちりす <mucchirisu@gmail.com>
2020-12-31 03:32:19 +0900

    ハンバーガーメニュー作成

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -7,10 +7,35 @@ $(function () {
     $('#hide-text').fadeOut();
   });
 
-  $('.l-header-nav-trigger').click(function () {
-    $('.l-header-nav__container').show();
+  $('.open').click(function () {
+    $('.l-header-nav__container').fadeIn(1000);
+  });
+  $('.open').click(function () {
+    $('.close').show();
+  });
+  $('.open').click(function () {
+    $(this).hide();
+  });
+  $('.close').click(function () {
+    $('.l-header-nav__container').fadeOut(1000);
+  });
+  $('.close').click(function () {
+    $('.open').show(1000);
+  });
+  $('.close').click(function () {
+    $(this).hide();
   });
 
+
+
+
+
+  // $('.open').on('click', '.open', function () {
+  //   $(this).removeClass('.open').addClass('.close');
+  // });
+  // $('body').on('click', '.box2', function () {
+  //   $(this).removeClass('box2').addClass('box3');
+  // });
 
 
 
