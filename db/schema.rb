172362d14ac3c27456c08c90a7f978f99a9a3578
むちりす <mucchirisu@gmail.com>
2021-01-04 00:24:12 +0900

    seeddateの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_032323) do
+ActiveRecord::Schema.define(version: 2020_11_24_133812) do
 
   create_table "newcomments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
