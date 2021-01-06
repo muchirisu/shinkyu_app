81752a304d26f77f5ce2fc4651ffbc2d4588f7a0
むちりす <mucchirisu@gmail.com>
2021-01-07 08:31:43 +0900

    emailnの形式で登録するためのvalidateasとpasswordを半角英小文字と数字の6文字以上12文字以内で登録するためのvalidatesを作成

--- app/models/newuser.rb
+++ app/models/newuser.rb
@@ -2,9 +2,16 @@ class Newuser < ApplicationRecord
   has_secure_password
 
   validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
-  # validates :content, length: { minimum: 1}
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)\z/
+  validates :email, {presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, {format: { with: VALID_PASSWORD_REGEX, message: "6~12文字の半角英小文字・数字それぞれ１文字以上含む必要があります"}}
+  validates :content, {length: { maximum: 200}}
   # validates :password_digest, format: { with: /\A\d+-\d+-\d+\z/}
+  
+
+
+
   
 
   def newposts
